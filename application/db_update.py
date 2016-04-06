3c2d5dbb5b44ba50ccd50275d30cb2aa98928b76
Scott Young <styoung@scotts-mbp.attlocal.net>
2016-04-05 21:43:43 -0700

    refactor updates

--- application/db_update.py
+++ application/db_update.py
@@ -1,12 +1,57 @@
 import db_connect
+from argparse import ArgumentParser
 
 
-if __name__ == '__main__':
-
+def games():
     data = db_connect.Twitch.run_fields()
 
     for index, row in enumerate(data):
         db_connect.update_table(row)
         print(index)
+
+
+def stream():
+    stream_row, team_row = db_connect.Twitch.run_streams()
+
+    for index, row in enumerate(stream_row):
+        db_connect.update_stream_table(row)
+        print index
+
+    for index, row in enumerate(team_row):
+        db_connect.update_team_table(row)
+        print index
+
+
+def featured():
+    stream_row, team_row = db_connect.Twitch.run_featured()
+
+    for index, row in enumerate(stream_row):
+        db_connect.update_stream_table(row)
+        print index
+
+    print 'updating tables'
+    for index, row in enumerate(team_row):
+        db_connect.update_team_table(row)
+        print index
+
+def parse_options():
+    parser = ArgumentParser()
+    parser.add_argument('-s', '--stream', default=False, action='store_true')
+    parser.add_argument('-f', '--featured', default=False, action='store_true')
+    parser.add_argument('-g', '--games', default=False, action='store_true')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    options = parse_options()
+    if options.games:
+        games()
+    if options.stream:
+        stream()
+    if options.featured:
+        featured()
+
+
+
 
 
