f8e2f8f36b262db3a5b3b8f320adb83d67eab8fb
Scott Young <styoung@Scotts-MacBook-Pro.local>
2016-04-29 17:07:43 -0700

    models bug

--- application/models.py
+++ application/models.py
@@ -1,7 +1,7 @@
 from application import app
-# from redis import ConnectionError
-# from worker import conn
-# from rq import Queue
+from redis import ConnectionError
+from worker import conn
+from rq import Queue
 from db import plots
 import json
 
@@ -9,7 +9,11 @@ import json
 def create_stream(name):
     div, stream = plots.run_stream(name)
 
-
+#    try:
+#        queue = Queue(connection=conn)
+#        queue.enqueue(plots.stream_model_data, stream)
+#    except ConnectionError:
+#        pass
     return div
 
 
